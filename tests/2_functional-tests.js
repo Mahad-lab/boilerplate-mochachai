e0be3d6b5664d1ee52f356aa4265b1a0670da431
Mahad Ahmed <Mahadahmed99@yahoo.com>
2024-12-07 14:31:10 +0000

    6 Functional - Submit the surname "Vespucci" in the HTML form

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -78,12 +78,12 @@ suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
   const browser = new Browser();
 
-  suiteSetup(function(done) {
+  suiteSetup(function (done) {
     return browser.visit('/', done);
   });
 
   suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
+    test('should have a working "site" property', function () {
       assert.isNotNull(browser.site);
     });
   });
@@ -104,9 +104,16 @@ suite('Functional Tests with Zombie.js', function () {
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', "Vespucci")
+      .then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        })
+      })
     });
   });
 });
